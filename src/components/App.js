418bf12a34e23f2c941d8b3dda1a092c6fbb2892
Ran Pol <rnd809@gmail.com>
2023-02-17 01:20:06 -0500

    Delete the hardcode Add card modal. Create the state variables for the modal and handle function for toggling the modal open status.

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,63 @@ import React from 'react'
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
-import closeIcon from '../images/CloseIcon.svg'
+import AddNewPlacePopup from './AddNewPlacePopup'
 
 const App = () => {
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
+    React.useState(false)
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false)
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
+    React.useState(false)
+
+  // Handle functions for opening/toggling the modals
+  const handleEditAvatarClick = () => {
+    setIsEditAvatarPopupOpen((prevs) => !prevs)
+  }
+  const handleEditProfileClick = () => {
+    setIsEditProfilePopupOpen((prevs) => !prevs)
+  }
+  const handleAddPlaceClick = () => {
+    setIsAddPlacePopupOpen((prevs) => !prevs)
+  }
+
+  // Handle onSubmit functions for the modals
+  const handleSubmitEditProfile = (e) => {
+    e.preventDefault()
+    console.log('Edit Profile')
+  }
+  const handleSubmitAddPlace = (e) => {
+    e.preventDefault()
+    console.log('Add Place')
+  }
+  const handleSubmitEditAvatar = (e) => {
+    e.preventDefault()
+    console.log('Edit Avatar')
+  }
+
   return (
     <div className="page">
       <Header />
-      <Main />
+      <Main
+        onEditProfileClick={handleEditProfileClick}
+        onAddPlaceClick={handleAddPlaceClick}
+        onEditAvatarClick={handleEditAvatarClick}
+        onCardClick={() => {
+          console.log('KLK')
+        }}
+      />
       <Footer />
+      
+      {isAddPlacePopupOpen && (
+        <AddNewPlacePopup
+          isAddPlacePopupOpen={isAddPlacePopupOpen}
+          onAddPlaceClick={handleAddPlaceClick}
+          handleSubmitAddPlace={handleSubmitAddPlace}
+        />
+      )}
+
       {/* <!-- Profile Modal --> */}
-      <div className="modal" id="modalEdit">
-        {/* <!-- Form --> */}
+      {/* <div className="modal" id="modalEdit">
         <form
           action="#"
           className="form modal__container"
@@ -60,7 +106,7 @@ const App = () => {
             >
               Save
             </button>
-            {/* <!-- className="profile__name-edit"  --> */}
+   
             <button
               aria-label="exit"
               className="form__button modal__button-close"
@@ -70,10 +116,9 @@ const App = () => {
             </button>
           </fieldset>
         </form>
-      </div>
+      </div> */}
       {/* <!--Edit Avatar Modal --> */}
-      <div className="modal" id="modalEditAvatar">
-        {/* <!-- Form --> */}
+      {/* <div className="modal" id="modalEditAvatar">
         <form
           action="#"
           className="form modal__container"
@@ -114,10 +159,9 @@ const App = () => {
             </button>
           </fieldset>
         </form>
-      </div>
+      </div> */}
       {/* <!-- Add Card Modal --> */}
-      <div className="modal" id="modalAdd">
-        {/* <!-- Form --> */}
+      {/* <div className="modal" id="modalAdd">
         <form
           action="#"
           className="form modal__container"
@@ -173,9 +217,9 @@ const App = () => {
             </button>
           </fieldset>
         </form>
-      </div>
+      </div> */}
       {/* <!-- Photo Expand Modal --> */}
-      <div className="modal" id="modalPicture">
+      {/* <div className="modal" id="modalPicture">
         <div className="modal__container">
           <img className="modal__picture-full" src="#" alt="expandedImage" />
           <p className="modal__piture-paragraph"></p>
@@ -187,9 +231,9 @@ const App = () => {
             <img src={closeIcon} alt="Close icon" className="modal__close" />
           </button>
         </div>
-      </div>
+      </div> */}
       {/* <!-- Delete Card Modal --> */}
-      <div className="modal" id="modalDelete">
+      {/* <div className="modal" id="modalDelete">
         <div className="modal__deletedcard modal__container">
           <h2 className="modal__confirmation-title">Are you sure?</h2>
           <button className="modal__button-yes">Yes</button>
@@ -201,7 +245,7 @@ const App = () => {
             <img src={closeIcon} alt="Close icon" className="modal__close" />
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
