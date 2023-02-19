7bbdb688f19b8c757131db6335785cdb4ca3f707
Ran Pol <rnd809@gmail.com>
2023-02-19 00:42:03 -0500

    Use context in Main and delete the userName, userDescription, and userAvatar state variables

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,8 @@ import Card from './CardTemplate/Card'
 import pencilEdit from '../images/Edit-Button.svg'
 import addIcon from '../images/add-button.svg'
 import api from '../utils/api'
+// Import the context value
+import CurrentUserContext from '../contexts/CurrentUserContext'
 
 const Main = ({
   onEditProfileClick,
@@ -11,20 +13,14 @@ const Main = ({
   onCardClick,
   handleCardDelete,
 }) => {
-  const [userName, setUserName] = React.useState('Username')
-  const [userRole, setUserRole] = React.useState('UserRole')
-  const [userAvatar, setUserAvatar] = React.useState(null)
+  // Get the value of the context
+  const { currentUser } = React.useContext(CurrentUserContext)
   const [cards, setCards] = React.useState([])
 
   React.useEffect(() => {
     api
-      .loadData()
-      .then(([userData, cardsData]) => {
-        console.log(userData)
-        console.log(cardsData)
-        setUserName(userData.name)
-        setUserRole(userData.about)
-        setUserAvatar(userData.avatar)
+      .getInitialCards()
+      .then((cardsData) => {
         setCards(cardsData)
       })
       .catch((err) => console.log(err))
@@ -34,49 +30,48 @@ const Main = ({
     <main className="main page__main">
       {/* <!-- Profile Section --> */}
       <section className="profile main__profile">
-        <div className="profile__container">
-          <div className="profile__avatar-container">
-            {userAvatar && (
+        {currentUser && (
+          <div className="profile__container">
+            <div className="profile__avatar-container">
               <img
-                src={userAvatar}
+                src={currentUser.avatar}
                 className="profile__avatar"
                 alt="User Avatar"
               />
-            )}
-            <div
-              className="profile__avatar-edit"
-              onClick={onEditAvatarClick}
-            ></div>
-          </div>
-
-          <div className="profile__namerole-container">
-            <div className="profile__name-container">
-              <h1 className="profile__name">{userName}</h1>
-              <button
-                aria-label="edit"
-                className="profile__name-edit"
-                type="button"
-                onClick={onEditProfileClick}
-              >
-                <img
-                  src={pencilEdit}
-                  alt="Pencil icon"
-                  className="profile__pencil"
-                />
-              </button>
+              <div
+                className="profile__avatar-edit"
+                onClick={onEditAvatarClick}
+              ></div>
             </div>
-            <p className="profile__role">{userRole}</p>
-          </div>
 
-          <button
-            aria-label="add"
-            type="button"
-            className="profile__add-button"
-            onClick={onAddPlaceClick}
-          >
-            <img src={addIcon} alt="Add icon" className="profile__add-icon" />
-          </button>
-        </div>
+            <div className="profile__namerole-container">
+              <div className="profile__name-container">
+                <h1 className="profile__name">{currentUser.name}</h1>
+                <button
+                  aria-label="edit"
+                  className="profile__name-edit"
+                  type="button"
+                  onClick={onEditProfileClick}
+                >
+                  <img
+                    src={pencilEdit}
+                    alt="Pencil icon"
+                    className="profile__pencil"
+                  />
+                </button>
+              </div>
+              <p className="profile__role">{currentUser.about}</p>
+            </div>
+            <button
+              aria-label="add"
+              type="button"
+              className="profile__add-button"
+              onClick={onAddPlaceClick}
+            >
+              <img src={addIcon} alt="Add icon" className="profile__add-icon" />
+            </button>
+          </div>
+        )}
       </section>
       {/* <!-- Cards Section --> */}
       <section className="cards">
